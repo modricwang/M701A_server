c6380dbee320a0ec7c007f3a7734ad7e7d898bc2
modricwang <modricwang@126.com>
2022-06-06 09:20:39 +0800

    add backend index

--- m701a/main.py
+++ m701a/main.py
@@ -1,14 +1,20 @@
+import sys
+
+from sqlalchemy.exc import OperationalError
+
 from m701a.io_utils import connect_helper
 import datetime
 import time
 from sqlalchemy import create_engine
 
-from sqlalchemy import Column
+from sqlalchemy import Column, Index
 from sqlalchemy import Integer, Float
 from sqlalchemy import String, DateTime
+# import sqlalchemy
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from argparse import ArgumentParser
+import signal
 
 Base = declarative_base()
 
@@ -24,7 +30,8 @@ class AirData(Base):
     PM10 = Column(Integer)
     Temperature = Column(Float)
     Humidity = Column(Float)
-    fullname = Column(String)
+
+    # fullname = Column(String)
 
     def __repr__(self):
         return "ID:%d, DateTime:%s, CO2: %d, CH2O:%d" % (self.id, self.time, self.CO2, self.CH2O)
@@ -48,27 +55,44 @@ def main():
 
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
+    try:
+        time_index = Index('time_index', AirData.time)
+        time_index.create(bind=engine)
+    except OperationalError:
+        pass
+
+    try:
+        id_index = Index('id_index', AirData.id)
+        id_index.create(bind=engine)
+    except OperationalError:
+        pass
+
     while True:
         try:
-            info = conn.read()
-        except IOError as e:
-            print(e)
-            conn.connect.close()
-            del conn
-            conn = connect_helper(port=args.port)
-            continue
-        print(datetime.datetime.now(), info)
-        time.sleep(30)
-        data = AirData(time=datetime.datetime.now(),
-                       CO2=info['CO2'],
-                       CH2O=info['CH2O'],
-                       TVOC=info['TVOC'],
-                       PM2_5=info['PM2.5'],
-                       PM10=info['PM10'],
-                       Temperature=info['Temperature'],
-                       Humidity=info['Humidity'])
-        session.add(data)
-        session.commit()
+            try:
+                info = conn.read()
+            except IOError as e:
+                print(e)
+                conn.connect.close()
+                del conn
+                conn = connect_helper(port=args.port)
+                continue
+            print(datetime.datetime.now(), info)
+            time.sleep(30)
+            data = AirData(time=datetime.datetime.now(),
+                           CO2=info['CO2'],
+                           CH2O=info['CH2O'],
+                           TVOC=info['TVOC'],
+                           PM2_5=info['PM2.5'],
+                           PM10=info['PM10'],
+                           Temperature=info['Temperature'],
+                           Humidity=info['Humidity'])
+            session.add(data)
+            session.commit()
+        except KeyboardInterrupt:
+            session.commit()
+            session.close()
+            break
 
 
 if __name__ == '__main__':
